5cc2618bbde9f8f82ed1952ad77aeff4810d0e69
rmhockey10 <rees.m.hannah@gmail.com>
2025-01-15 13:39:00 -0500

    uploading updated block08 Soltuions .js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let code1;
 code1 = 7 + 3;
-let code2 = 84 / 2 - 2;
+let code2 = 86 / 2 - 3;
 let code3 = 79 % 40;
 let message = "The vault has been secured.  The comination is:";
 let codeA = code1 + "-" + code2 + "-" + code3;
